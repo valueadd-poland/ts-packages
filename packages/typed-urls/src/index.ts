4de4090b1c3a6bd37d659a5b67d85da036c79920
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-10-22 09:50:38 +0200

    refactor(typed-urls): remove tslib
    Remove "export *" to prevent tslib
    dependency after build.
    Add CONTRIBUTING.md

--- packages/typed-urls/src/index.ts
+++ packages/typed-urls/src/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/resources/models/interpolatable-url.model';
-export * from './lib/resources/models/url.model';
-export * from './lib/resources/utils/url-factory/url.factory';
+export { InterpolatableUrl } from './lib/resources/models/interpolatable-url.model';
+export { Url } from './lib/resources/models/url.model';
+export { urlFactory } from './lib/resources/utils/url-factory/url.factory';
